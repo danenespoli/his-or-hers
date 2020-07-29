b2b5c9cfdaa9bc3818b20c677335a4702791494e
Dane Nespoli <danenespoli@gmail.com>
2020-07-28 22:58:36 -0400

    game

---
+++ src/client/Game.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import './home.css';
+import { Button } from 'evergreen-ui';
+
+export default class Home extends Component {
+  state = {
+    question: 'There is currently no question!',
+    timeLeft: 30,
+  };
+
+  submitGuess(guess) {
+    console.log(guess);
+  }
+
+  render() {
+    const { question, timeLeft } = this.state;
+
+    return (
+      <div>
+        <div>
+          {question}
+        </div>
+        <div>
+          Time left:
+          {timeLeft}
+        </div>
+        <div>
+          <Button onClick={() => this.submitGuess('his')}>
+            His
+          </Button>
+          <Button onClick={() => this.submitGuess('hers')}>
+            Hers
+          </Button>
+        </div>
+      </div>
+    );
+  }
+}
