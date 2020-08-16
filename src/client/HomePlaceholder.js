e475affaf0f289efae96e1b370e9a973ca6d500d
Dane Nespoli <danenespoli@gmail.com>
2020-08-16 12:30:47 -0400

    add face images

--- src/client/HomePlaceholder.js
+++ src/client/HomePlaceholder.js
@@ -74,10 +74,14 @@ class HomePlaceholder extends Component {
   }
 
   render() {
-    const { name, errorMsg, backgroundNum } = this.state;
+    const { backgroundNum } = this.state;
 
     return (
       <div className={`home-background home-background-${backgroundNum}`}>
+        <div className="home-images">
+          <img className="home-image-hers" src={require('./img/her-faces-02-pix.png')} />
+          <img className="home-image-his" src={require('./img/his-faces-05-pix.png')} />
+        </div>
         <div className="home-text-container">
           <div className={`home-text home-text-${backgroundNum}`}>Steph or Dustin</div>
         </div>
