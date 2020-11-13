35f1fe4d95a411bb7fe9096eb0c76907d33b9b9f
Dane Nespoli <danenespoli@gmail.com>
2020-11-12 22:01:00 -0500

    wip

--- src/client/GameState.js
+++ src/client/GameState.js
@@ -1,76 +1,16 @@
 import { atom } from 'recoil';
 
-const joinedState = atom({
-  key: 'joined',
-  default: false,
+export default atom({
+  joined: false,
+  question: null,
+  questionNum: 1,
+  questionTotal: 1,
+  answer: null,
+  guess: null,
+  time: 30,
+  score: 0,
+  topScores: null,
+  finalScore: null,
+  theme: 4,
+  name: null,
 });
-
-const questionState = atom({
-  key: 'question',
-  default: null,
-});
-
-const questionNumState = atom({
-  key: 'questionNum',
-  default: 1,
-});
-
-const questionTotalState = atom({
-  key: 'questionTotal',
-  default: 1,
-});
-
-const answerState = atom({
-  key: 'answer',
-  default: null,
-});
-
-const guessState = atom({
-  key: 'guess',
-  default: null,
-});
-
-const timeState = atom({
-  key: 'time',
-  default: 30,
-});
-
-const scoreState = atom({
-  key: 'score',
-  default: 0,
-});
-
-const topScoresState = atom({
-  key: 'topScores',
-  default: null,
-});
-
-const finalScoreState = atom({
-  key: 'finalScore',
-  default: null,
-});
-
-const themeState = atom({
-  key: 'theme',
-  default: 4,
-});
-
-const nameState = atom({
-  key: 'name',
-  default: null,
-});
-
-export {
-  joinedState,
-  questionState,
-  questionNumState,
-  questionTotalState,
-  answerState,
-  guessState,
-  timeState,
-  scoreState,
-  topScoresState,
-  finalScoreState,
-  themeState,
-  nameState,
-};
